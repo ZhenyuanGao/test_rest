84c2995e3f188f7da2e914b9004d7f4098e032c5
ZhenyuanGao <kegao@westmont.edu>
2022-06-14 14:43:30 -0700

    here

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React this is kvin hahahahahahahahh
         </a>
       </header>
     </div>
